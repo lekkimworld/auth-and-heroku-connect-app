f087aa1f452685c8edc98e288b90c41774995c6c
Mikkel Flindt Heisterberg <mheisterberg@salesforce.com>
2020-04-23 23:46:49 +0200

    Fix ordering

--- index.js
+++ index.js
@@ -10,20 +10,20 @@ if (myutils.authenticationConfigured()) {
     passport.use(new Strategy({
         "authorizationURL": (function() {
             if (process.env.AUTH_URL) return process.env.AUTH_URL;
-            if (process.env.MY_DOMAIN) return `https://${process.env.MY_DOMAIN}.my.salesforce.com/services/oauth2/authorize`;
             if (process.env.SANDBOX) return "https://test.salesforce.com/services/oauth2/authorize";
+            if (process.env.MY_DOMAIN) return `https://${process.env.MY_DOMAIN}.my.salesforce.com/services/oauth2/authorize`;
             return undefined;
         })(),
         "tokenURL": (function() {
             if (process.env.TOKEN_URL) return process.env.TOKEN_URL;
-            if (process.env.MY_DOMAIN) return `https://${process.env.MY_DOMAIN}.my.salesforce.com/services/oauth2/token`;
             if (process.env.SANDBOX) return "https://test.salesforce.com/services/oauth2/token";
+            if (process.env.MY_DOMAIN) return `https://${process.env.MY_DOMAIN}.my.salesforce.com/services/oauth2/token`;
             return undefined;
         })(),
         "profileURL": (function() {
             if (process.env.PROFILE_URL) return process.env.PROFILE_URL;
-            if (process.env.MY_DOMAIN) return `https://${process.env.MY_DOMAIN}.my.salesforce.com/services/oauth2/userinfo`;
             if (process.env.SANDBOX) return "https://test.salesforce.com/services/oauth2/userinfo";
+            if (process.env.MY_DOMAIN) return `https://${process.env.MY_DOMAIN}.my.salesforce.com/services/oauth2/userinfo`;
             return undefined;
         })(),
         "clientID": process.env.CLIENT_ID,
