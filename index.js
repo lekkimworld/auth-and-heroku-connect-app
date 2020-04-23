c30029ff8de1f345ea54f80825267937be6c4b4f
Mikkel Flindt Heisterberg <mheisterberg@salesforce.com>
2020-04-23 23:39:52 +0200

    Explicit url options as well

--- index.js
+++ index.js
@@ -8,9 +8,24 @@ const myutils = require("./myutils");
 // configure Salesforce OAuth if present
 if (myutils.authenticationConfigured()) {
     passport.use(new Strategy({
-        "authorizationURL": process.env.MY_DOMAIN ? `https://${process.env.MY_DOMAIN}.my.salesforce.com/services/oauth2/authorize` : process.env.SANDBOX ? "https://test.salesforce.com/services/oauth2/authorize" : undefined,
-        "tokenURL": process.env.MY_DOMAIN ? `https://${process.env.MY_DOMAIN}.my.salesforce.com/services/oauth2/token` : process.env.SANDBOX ? "https://test.salesforce.com/services/oauth2/token" : undefined,
-        "profileURL": process.env.MY_DOMAIN ? `https://${process.env.MY_DOMAIN}.my.salesforce.com/services/oauth2/userinfo` : process.env.SANDBOX ? "https://test.salesforce.com/services/oauth2/userinfo" : undefined,
+        "authorizationURL": (function() {
+            if (process.env.AUTH_URL) return process.env.AUTH_URL;
+            if (process.env.MY_DOMAIN) return `https://${process.env.MY_DOMAIN}.my.salesforce.com/services/oauth2/authorize`;
+            if (process.env.SANDBOX) return "https://test.salesforce.com/services/oauth2/authorize";
+            return undefined;
+        })(),
+        "tokenURL": (function() {
+            if (process.env.TOKEN_URL) return process.env.TOKEN_URL;
+            if (process.env.MY_DOMAIN) return `https://${process.env.MY_DOMAIN}.my.salesforce.com/services/oauth2/token`;
+            if (process.env.SANDBOX) return "https://test.salesforce.com/services/oauth2/token";
+            return undefined;
+        })(),
+        "profileURL": (function() {
+            if (process.env.PROFILE_URL) return process.env.PROFILE_URL;
+            if (process.env.MY_DOMAIN) return `https://${process.env.MY_DOMAIN}.my.salesforce.com/services/oauth2/userinfo`;
+            if (process.env.SANDBOX) return "https://test.salesforce.com/services/oauth2/userinfo";
+            return undefined;
+        })(),
         "clientID": process.env.CLIENT_ID,
         "clientSecret": process.env.CLIENT_SECRET,
         "callbackURL": process.env.REDIRECT_URI
